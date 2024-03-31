f13aeb7c817112986e6f42d8a2e3750922d3aaf4
Gurubaran-B <70705638+Gurubaran-B@users.noreply.github.com>
2024-03-31 17:50:50 +0530

    Update Card.js

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -5,7 +5,7 @@ function Card({title, logo, description, image, color}) {
     return (
         <div className={`${style.container}`} style={{backgroundColor: color}}>
             <div className={`${style.container_l}`}>
-                <div className={`${style.logo_container}`}><img src={logo} className={`${style.logo}`}/></div>
+                <div className={`${style.logo_container}`}><img src={logo} className={`${style.logo}`} alt='company_logo'/></div>
                 <div className={`${style.title_container}`}>{title}</div>
                 <hr className={`${style.divider}`}/>
                 <div className={`${style.description}`}>{description}</div>
@@ -13,7 +13,7 @@ function Card({title, logo, description, image, color}) {
 
             <div className={`${style.container_r}`}>
                 <div className={`${style.image_wrapper}`}>
-                    <img src={image} className={`${style.image}`}/>
+                    <img src={image} className={`${style.image}`} alt='Product_image'/>
                 </div>
                 
             </div>
